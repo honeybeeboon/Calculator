e14e52973e3b0947ae1c4debfc04013eb1313bfc
kazuki <firstshining.flowerflower.yui@gmail.com>
2019-07-15 09:46:34 +0900

    add ui view

--- Sample/ContentView.swift
+++ Sample/ContentView.swift
@@ -9,10 +9,78 @@
 import SwiftUI
 
 struct ContentView : View {
+    @State var num:Int = 1
+    @State var showText = false
+    let data = [["AC", "+/-", "%", "÷"],
+                ["7", "8", "9", "×"],
+                ["4", "5", "6", "ー"],
+                ["1", "2", "3", "+"],
+                ["0", ".", "="]]
+    let dataTop = ["AC", "+/-","%"]
+    let dataCalc = ["÷","×","ー","+","="]
+    @State private var userIsInTheMiddleOfTyping = false
+    @State private var display = "0"
     var body: some View {
-        Text("Hello World")
+        let margin: CGFloat = 10
+        return ZStack(alignment: .center){
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Spacer()
+                    
+                    Text(display)
+                        .foregroundColor(Color.white)
+                        .font(.custom("Roboto-Light",size: 80))
+                        .frame(height: 50)
+                    }
+                    .padding(margin)
+                VStack(alignment: .trailing, spacing: margin) {
+                    ForEach(data.identified(by: \.description)) { items in
+                        HStack(alignment: .center, spacing: margin) {
+                            ForEach(items.identified(by: \.description)) { item in
+                                Button(action: {}) {
+                                    if self.dataTop.contains(item) {
+                                        Text(item)
+                                            .color(Color.black)
+                                            .font(.largeTitle)
+                                            .frame(width: 90.0, height: 90.0)
+                                            .background(Color.gray,cornerRadius: Length(100))
+                                    }else if self.dataCalc.contains(item){
+                                        Text(item)
+                                            .color(Color.white)
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                            .frame(width: 90.0, height: 90.0)
+                                            .background(Color.orange,cornerRadius: Length(100))
+                                    }else if item == "0" {
+                                        Text(item)
+                                            .color(Color.white)
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                            .frame(width: 180.0, height: 90.0)
+                                            .background(Color(red: 79/255, green: 79/255, blue: 79/255),cornerRadius: Length(100))
+                                        
+                                    }else {
+                                        Text(item)
+                                            .color(Color.white)
+                                            .font(.largeTitle)
+                                            .fontWeight(.bold)
+                                            .frame(width: 90.0, height: 90.0)
+                                            .background(Color(red: 79/255, green: 79/255, blue: 79/255),cornerRadius: Length(100))
+                                    }
+                                }
+                            }
+                            }
+                            .padding(5)
+                    }
+                }
+                
+            }
+        }
     }
 }
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
